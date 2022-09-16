810fac3e7524ddf2861516bd15848e7b9baf727d
hamerzin <dipaolo@gmail.com>
2022-09-16 11:50:43 -0300

    nuevo2

--- btre/settings.py
+++ btre/settings.py
@@ -144,9 +144,9 @@ MESSAGE_TAGS = {
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_STORAGE= 'whitenoise.django.GziManifestStaticFilesStorage'
 # Update database configuration with $DATABASE_URL.
-#import dj_database_url  
-#db_from_env = dj_database_url.config(conn_max_age=500)  
-#DATABASES['default'].update(db_from_env)
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)
 
 try:
     from .local_settings import *
